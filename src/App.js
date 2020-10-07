81923e249d96af461e475ab8647f538189e53e86
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-06 21:54:15 -0600

    Make each movie an object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+// import { getMovieData } from './API.js';
+import API from './API.js';
+import MovieData from './Movie.js';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+    <h1 className='Apps'>
+      {props.movies.map(movie =>
+        <MovieData key={movie.title} movie={movie}/>
+      )}
+    </h1>
+    </section>
+  )
 }
+
 
 export default App;
