cf56a8101b15a35f55a24831b96e4e7b48bff67a
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-11 19:05:00 -0600

    Add ShowPage class

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import { Component } from 'react';
+import { getOneMovie } from '../apiFetch.js'
 
 class ShowPage extends Component {
-  constructor() {
+  constructor(props) {
+    super(props);
     this.state = {
-      singleMovie: [],
+      id: 0,
+      title: '',
+      poster_path: '',
+      backdrop_path: '',
+      release_date: '',
+      overview: '',
+      genres: [],
+      budget: 0,
+      revenue: 0,
+      runtime: 0,
+      tagline: '',
+      average_rating: 0
     }
   }
-  componentDidMount() {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/'+this.state.singleMovie.id)
-      .then(response => response.json())
-      .then(data => this.setState({ singleMovie: data.movie}))
+  // componentDidMount() {
+  //   fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/'+this.state.singleMovie.id)
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ singleMovie: data.movie}))
+  // }
+
+  fetchSingleMovie = () => {
+    getOneMovie(this.state.id)
   }
 
   render() {
-    const background = {
-      backgroundImage: 'url(' + this.state.singleMovie.backdropPath + ')'
-    }
+    // const background = {
+    //   backgroundImage: 'url(' + this.state.singleMovie.backdrop_path + ')'
+    // }
+    console.log(this.state)
+    console.log('id', this.state.id)
     return (
       <div>
-        <section className='single-movie' style= { background }>
-        <p className='single-movie-title'>{this.state.singleMovie.title}</p>
+        <section className='single-movie'>
+        <p className='single-movie-title'>{this.state.title}</p>
         </section>
       </div>
     )
