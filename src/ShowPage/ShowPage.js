5dbf5046df9c84e1240a49a653c5dfedb13da748
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-13 19:54:24 -0600

    Format individual movie pages

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -70,11 +70,34 @@ class ShowPage extends Component {
     console.log("HELLO")
     console.log(this.state)
     console.log('render-id', this.state.id)
+    let imgUrl = this.state.backdrop_path
     return (
       <div>
-        <section className='single-movie'>
-        <p className='test-script'>This is the Single Movie Page</p>
-        <p className='single-movie-title'>Title:{this.state.title}</p>
+        <section className='single-movie-area'
+          style = {{ backgroundImage: `url(${imgUrl})`,
+            backgroundSize: 'auto 100%',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+            backgroundAttachment: 'fixed',
+              }}>
+          <section className='movie-main-top'>
+            <section className='movie-main-top-left'>
+              <h2 className='single-movie-title'>{this.state.title}</h2>
+              <p className='single-movie-tagline'>{this.state.tagline}</p>
+              <p className='single-movie-release-date'><a className='single-movie-info'>Release Date:</a> {this.state.release_date}</p>
+              <p className='single-movie-rating'><a className='single-movie-info'>Average Rating:</a> {this.state.average_rating.toFixed(2)}/10</p>
+              <p className='single-movie-genre'><a className='single-movie-info'>Genre:</a> {this.state.genres}</p>
+              <p className='single-movie-budget'><a className='single-movie-info'>Budget:</a> ${this.state.budget}</p>
+              <p className='single-movie-revenue'><a className='single-movie-info'>Revenue:</a> ${this.state.revenue}</p>
+              <p className='single-movie-runtime'><a className='single-movie-info'>Runtime:</a> {this.state.runtime} minutes</p>
+            </section>
+            <section className='movie-main-top-right'>
+              <img className='single-movie-poster' src={this.state.poster_path}/>
+            </section>
+          </section>
+          <section className='movie-main-bottom'>
+            <p className='movie-main-overview'>{this.state.overview}</p>
+          </section>
         </section>
       </div>
     )
