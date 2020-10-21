198f35f090b2bfe57e7cc5b28eca9cc00f393a4c
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-20 21:24:29 -0600

    Format Header, add logout

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,16 +7,7 @@ class Header extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      headerMessage: 'Welcome!'
-    }
-  }
-
-
-  setHeaderMessage() {
-    console.log("SET HEADER MESSAGE IS RUNNING");
-    if (this.props.user.name) {
-      console.log('USER PROP', this.props.user)
-    this.setState({ headerMessage: `Welcome, ${this.props.user.name}!`})
+      
     }
   }
 
@@ -27,16 +18,12 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-      
       <header className='header'>
-        <a className="header-text header-link" href='https://github.com/kathrynljackson/Rancid-Tomatillos-KK'>About</a>
         <a className="header-text header-link" href='/login'>Login</a>
-        <a className="header-text header-link" href='/' onClick={this.logout()}>Logout</a>
+        <a className="header-text header-link" href='https://github.com/kathrynljackson/Rancid-Tomatillos-KK'>About</a>
+        <a className="header-text header-link" href='/' onClick={this.logout()} style={{ visibility: this.props.user.id > 0 ? 'visible' : 'hidden' }}>Logout</a>
         <a className="header-text rotten-tomatillos">Rotten Tomatillos</a>
       </header>
-      <section className="header-text login-message">{this.state.headerMessage}</section>
-      </div>
     )
   }
 }
