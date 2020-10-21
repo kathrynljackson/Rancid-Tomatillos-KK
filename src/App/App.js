ed65e7021c4a92b5860649411372c34b5b5fd4ed
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-20 21:08:20 -0600

    Remove unused code

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
-// //import logo from './logo.svg';
 import './App.css';
-// import ReactDOM from 'react-dom';
-// // import { getMovieData } from './API.js';
-// // import API from './API.js';
 import Header from '../Header/Header.js';
 import MovieData from '../MovieData/MovieData.js';
-import Login from '../Login/Login.js'
-import ShowPage from '../ShowPage/ShowPage.js'
-import { getRatings} from '../apiFetch.js'
-
-// import React, { Component } from 'react';
+import Login from '../Login/Login.js';
+import ShowPage from '../ShowPage/ShowPage.js';
+import { getRatings} from '../apiFetch.js';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
-//
-//
 
 class App extends Component {
   constructor() {
@@ -35,17 +27,6 @@ class App extends Component {
 
   }
 
-  // userMessage = () => {
-  //   if (this.state.user) {
-  //     console.log('MESSAGE', this.state.currentUser)
-  //     return <h2>Welcome, {this.state.user.name}!</h2>
-      
-  //   } else {
-
-  //     return <h2>No User</h2>
-  //   }
-  // }
-
   loginView = () => {
     if (!this.state.user.name) {
       this.setState({loginStatus: true})
@@ -60,15 +41,11 @@ class App extends Component {
   }
 
   render = () => {
-    /*let welcomeMessage = this.userMessage()*/
     return (
       <main className='App'>
         <Route exact path='/'>
           <Header user={this.state.user} />
           <MovieData user={this.state.user}/>
-        </Route>
-        <Route exact path='/login' >
-          <h1>HELLO</h1>
         </Route>
         <Route exact path='/movies' >
           <Header user={this.state.user} />
@@ -76,7 +53,6 @@ class App extends Component {
         </Route>
         <Route exact path='/login' render={ () => <Login setUser={this.setCurrentUser} userId={this.state.user.id}/> }
         />
-
         <Route exact path='/movies/:movie_id'
         render={({ match }) => {
           const { movie_id } = match.params;
@@ -87,38 +63,6 @@ class App extends Component {
     )
   }
 }
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <div>
-//       <Switch>
-//         <Route path="/login" component={Login} />
-//         <Route path="/movie" component={MovieData} />
-//         </Switch>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-
-// function App() {
-//   return (
-//     <main>
-//         <Switch>
-//             <Route exact path="/" render= {() => {
-//               return <Login />;}
-//             }
-//               exact />
-//             <Route path="/movie" render= {() => {
-//               return <MovieData />;}
-//             }
-//             exact />
-//             // <Route component={Error} />
-//         </Switch>
-//     </main>
-//   )
-// }
 
 
 
